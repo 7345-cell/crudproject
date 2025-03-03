9fe8ccd295f8383b7e66b82626630e5ceab96a7f
Evance <evanesh7345@gmail.com>
2025-03-03 09:31:03 +0300

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,redirect
+from .models import User
+from .forms import UserForm
+
+# Create your views here.
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request,'listingpage.html',{'users':users})
+
+def add_user(request):
+    mydict={}
+    form = UserForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    mydict['form'] = form
+    return render(request,'add.html',mydict)
